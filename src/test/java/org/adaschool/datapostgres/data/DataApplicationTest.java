895fce0ad5749da325702595d31f37522ffd5e38
Henri Galdámez <estuardo.galdamezedumail.com>
2024-08-17 15:25:53 -0600

    first commit

---
+++ src/test/java/org/adaschool/datapostgres/data/DataApplicationTest.java
@@ -0,0 +1,13 @@
+package org.adaschool.datapostgres.data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
